27e069bb3981d2565afa761d2e7b4f92586a9191
mheon <mheon@ciena.com>
2016-11-06 08:01:43 -0500

    adding controller

--- app/pods/application/route.js
+++ app/pods/application/route.js
@@ -1,5 +1,8 @@
 import Ember from 'ember'
 
 export default Ember.Route.extend({
-
+  setupController: function (controller, model) {
+    this._super(...arguments)
+    controller.test = new AudioContext()
+  }
 })
